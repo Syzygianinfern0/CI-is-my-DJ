307d051b7b2f34f169424d7d2926f5463110500a
Syzygianinfern0 <spsharan2000@gmail.com>
2022-12-01 23:40:07 -0600

    Refactor complete project to OrderedDict

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import itertools
 import json
 from argparse import ArgumentParser
+from collections import OrderedDict
 
 import gspread
 import spotipy
-from ordered_set import OrderedSet
 from spotipy.oauth2 import SpotifyOAuth
 
 SOURCES = {
@@ -52,38 +51,60 @@ def main():
     gc = gspread.service_account(filename=args.c)
     worksheet = gc.open("CI Is My DJ").sheet1
     db_tracks = worksheet.get_values()
-    db_tracks = OrderedSet([tuple(track) for track in db_tracks])
-    print(f"Found {len(db_tracks)} entries in DB")
+    db_tracks = OrderedDict({url: [title, [each for each in origin if len(each)]] for url, title, *origin in db_tracks})
+    num_existing = len(db_tracks)
+    print(f"Found {num_existing} entries in DB")
 
     # Aggregate all songs
     creds = json.load(open("credentials.json"))
     id, secret = creds["spotify_client_id"], creds["spotify_client_secret"]
     scope = "playlist-modify-public"
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(id, secret, "http://example.com", scope=scope))
-    tracks = [get_all_tracks(sp, playlist) for playlist in SOURCES.values()]
-    tracks = itertools.chain.from_iterable(tracks)
-    tracks = [[each["track"]["external_urls"]["spotify"], each["track"]["name"]] for each in tracks]
-    tracks = OrderedSet([tuple(track) for track in tracks])  # remove duplicates
-    print(f"Scraped {len(tracks)} unique tracks in total")
 
-    # Find new songs
-    new_tracks = tracks - db_tracks
-    print(f"Found {len(new_tracks)} new tracks in total")
+    updated_idx = []
+    new_idx = []
+    for playlist in SOURCES.values():
+        playlist_name = sp.playlist(playlist)["name"]
+        tracks = get_all_tracks(sp, playlist)
+        for track in tracks:
+            url = track["track"]["external_urls"]["spotify"]
+            if url in db_tracks.keys():
+                if playlist_name not in db_tracks[url][1]:
+                    db_tracks[url][1].append(playlist_name)
+                    updated_idx.append(list(db_tracks.keys()).index(url))
+            else:
+                db_tracks[url] = [track["track"]["name"], [playlist_name]]
+                new_idx.append(list(db_tracks.keys()).index(url))
 
     if not args.d:
         # Update Playlist
-        # TODO: Implement checking for DB and playlists
-        urls = [each[0] for each in new_tracks.items]
-        [sp.playlist_add_items(TARGET, urls[idx : idx + 100]) for idx in range(0, len(urls), 100)]
+        if len(new_idx):
+            urls = [each[0] for each in list(db_tracks.items())[new_idx[0] :]]
+            [sp.playlist_add_items(TARGET, urls[idx : idx + 100]) for idx in range(0, len(urls), 100)]
 
-        # TODO: Implement auto monthly digests
-        # dec_id = "2emmetze5gmxnAhF2mt0x6"
-        # [sp.playlist_add_items(dec_id, urls[idx : idx + 100]) for idx in range(0, len(urls), 100)]
-        # print(f"Updated target playlist with {len(new_tracks)} tracks")
+            # TODO: Implement auto monthly digests
+            dec_id = "2emmetze5gmxnAhF2mt0x6"
+            [sp.playlist_add_items(dec_id, urls[idx : idx + 100]) for idx in range(0, len(urls), 100)]
 
         # Update DB
-        worksheet.update(f"A{len(db_tracks) + 1}", new_tracks.items)
-        print(f"Updated database and playlists with {len(new_tracks)} new tracks")
+        if len(new_idx):
+            worksheet.update(
+                f"A{new_idx[0] + 1}",
+                [
+                    [each[0], each[1], *each[2]]
+                    for each in [[key, *value] for key, value in list(db_tracks.items())[new_idx[0] :]]
+                ],
+            )
+        updated_idx = list(set(updated_idx) - set(new_idx))
+        for idx in updated_idx:
+            worksheet.update(
+                f"A{idx + 1}",
+                [
+                    [each[0], each[1], *each[2]]
+                    for each in [[key, *value] for key, value in [list(db_tracks.items())[idx]]]
+                ],
+            )
+        print(f"Updated database and playlists with {len(new_idx)} new tracks and {len(updated_idx)} updates")
 
 
 if __name__ == "__main__":
